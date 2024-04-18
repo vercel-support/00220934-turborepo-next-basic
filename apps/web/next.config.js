52c3932c6a08e26549046d01d493bcac43563ab3
Nick Vigier <nvigier46@gmail.com>
2024-04-18 12:38:17 +0100

    Add rewrite rule

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   transpilePackages: ["@repo/ui"],
+  rewrites: async () => {
+    return [
+      {
+        source: "/blog/:path*",
+        destination: "https://00220934-turborepo-next-basic-docs.vercel.app/blog/:path*",
+      },
+    ];
+  }
 };
