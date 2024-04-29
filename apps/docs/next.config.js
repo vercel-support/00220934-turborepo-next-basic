e9d6ea53056937ce5a1dc5aa35145cfacd557388
Nick Vigier <nvigier46@gmail.com>
2024-04-29 09:02:31 +0100

    Use redirects in next.config.js

--- apps/docs/next.config.js
+++ apps/docs/next.config.js
@@ -2,4 +2,14 @@
 module.exports = {
   transpilePackages: ["@repo/ui"],
   basePath: "/blog",
+  redirects: async () => {
+    return [
+      {
+        source: "/((?!blog).*)",
+        destination: "https://00220934.nicolas-vigier.com",
+        basePath: false,
+        permanent: true,
+      },
+    ];
+  }
 };
